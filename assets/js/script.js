d9abd5eecdfd425c65fa32c0456e7deca9adc4e9
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2019-04-13 21:31:09 -0500

    made this clock for learning purposes

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const clock = document.getElementById('clock');
+
+function updateTime () {
+
+const now = moment();
+const humanReadable = now.format('hh:mm:ssA');
+
+clock.textContent = humanReadable;
+}
+
+setInterval(updateTime, 1000);
+updateTime();
